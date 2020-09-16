50d9284f978362c192f295a0799ca914fc228b24
Fanotik <enotikfanotik@gmail.com>
2020-09-16 10:11:12 +0300

    Initial commit

---
+++ src/TestAuthor.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("R.R. Martin", "Martin@martin.art",'m');
+        System.out.println(a);
+    }
+}
